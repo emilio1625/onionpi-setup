dd1842ac29fc446d5ac9557bb04bc97f70140a19
Dan Shuman <StarshipEngineer@users.noreply.github.com>
2017-03-21 12:37:16 -0500

    Removes spaces in tor script

--- tor/torsetup.sh
+++ tor/torsetup.sh
@@ -7,19 +7,20 @@ sudo apt-get install -y tor
 # Configure Tor
 
 sed -i '/faq#torrc/a\
-Log  notice  file  /var/log/tor/notices.log\
-VirtualAddrNetwork  10.192.0.0/10\
-AutomapHostsSuffixes  .onion,.exit\
-AutomapHostsOnResolve  1\
-TransPort  9040\
-TransListenAddress  192.168.42.1 DNSPort  53\
-DNSListenAddress  192.168.42.1
+Log notice file /var/log/tor/notices.log
+VirtualAddrNetwork 10.192.0.0/10
+AutomapHostsSuffixes .onion,.exit
+AutomapHostsOnResolve 1
+TransPort 9040
+TransListenAddress 192.168.42.1
+DNSPort 53
+DNSListenAddress 192.168.42.1
 ' /etc/tor/torrc
 
 
 # Flush previous iptables rules
-sudo  iptables  -F
-sudo  iptables  -t  nat  -F
+sudo iptables -F
+sudo iptables -t nat -F
 
 # Set new iptables rules
 
@@ -29,13 +30,13 @@ sudo iptables -t nat -A PREROUTING -i wlan0 -p udp --dport 53 -j REDIRECT --to-p
 
 sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --syn -j REDIRECT --to-ports 9040
 
-sudo  sh  -c  "iptables-save  >  /etc/iptables.ipv4.nat"
+sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
 
 # Create a log file
-sudo touch  /var/log/tor/notices.log
-sudo chown  debian-tor  /var/log/tor/notices.log
-sudo chmod  644  /var/log/tor/notices.log
+sudo touch /var/log/tor/notices.log
+sudo chown debian-tor /var/log/tor/notices.log
+sudo chmod 644 /var/log/tor/notices.log
 
 # Start Tor and enable it on boot
-sudo  service  tor  start
-sudo  update-rc.d  tor  enable
+sudo service tor start
+sudo update-rc.d tor enable
